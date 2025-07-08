c432cd6b46a07e5240ff2d4b957e02f3615952fe
MeeSumee <stevemorrow69420@gmail.com>
2025-07-07 20:57:10 -0500

    Update cursors.nix to latest rev

--- pkgs/cursors.nix
+++ pkgs/cursors.nix
@@ -4,14 +4,14 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     repo = "Greenery";
     owner = "MeeSumee";
-    rev = "b3e311464be47f023243229e853968a5670b9952";
-    hash = "sha256-7kIz3fqEeGDTVeM8nUpWxRQp9kwVpGqfx+CD+YKVT90=";
+    rev = "cc9b437c88bb2bf1f1b46507f1df8964c058d215";
+    hash = "sha256-iKawpcP5JzEpfdv5OiUPa38K2BkbNSfkWfR/vLEfie8=";
   };
 
   installPhase = ''
     mkdir -p $out/share/icons/xcursor-genshin-nahida
-    cp -r ./cursors/xcursor-genshin-nahida/* $out/share/icons/xcursor-genshin-nahida
+    cp -r ./dots/cursors/xcursor-genshin-nahida/* $out/share/icons/xcursor-genshin-nahida
     mkdir -p $out/share/icons/Firefly
-    cp -r ./cursors/Firefly/* $out/share/icons/Firefly
+    cp -r ./dots/cursors/Firefly/* $out/share/icons/Firefly
   '';
 }
