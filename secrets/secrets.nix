011f048e1b1639fe912b7d40a28b06d822a7ef49
MeeSumee <stevemorrow69420@gmail.com>
2025-08-18 13:27:21 -0500

    fix merge conflict

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   users = {
     administrator = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHG2SPbpKOYi1kLAIOZGz4Tb2F2okroCt3J2Wq5XrMys"];
-    sumeezome = [""];
+    sumeezome = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwTjZGFn9J8wwwSAxfIirryeMBBLofBNF7fZ40engRh"];
     sumee = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHITLg3/cEFB883XDG1KnaSmEAkYbqOBJMziWmfEadqO sumee@quartz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8XrDxEKnoEvZTau4shhCKKsQlW7W3AD0xymhO1sTpF sumee@kaolin" 
@@ -12,7 +12,7 @@ let
     greenery = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiA3lkgGECrzk08GOhUlSIx5+jQ6WvuERK3nAz617M7"];
     quartz = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCHbUX3L2WxbS3d1EeCe+WhzdT1Tn78MzAzOz5xr1iO"];
     kaolin = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID4CgVhTfEtERQsj2rIQVjLMS6vpXTBi/K2GBMHOzCUN"];
-    beryl = [""];
+    beryl = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/j0qkT/J69KlyUNFx6sDILbCf4g20MxXBd10YDvwrd"];
   };
 
 in {
