7f85f85841df74ec257a194e0735bb0679102c0c
MeeSumee <stevemorrow69420@gmail.com>
2025-09-30 18:15:13 -0500

    remove khal

--- modules/programs/nixpkgs.nix
+++ modules/programs/nixpkgs.nix
@@ -75,7 +75,6 @@
         wlsunset                        # I need fucking blue light filter, my fucking eyes hurt
         swww                            # SWWW wallpaper daemon
         ddcutil                         # Manipulating external monitors using i2c bus
-        khal                            # CLI Calendar dependency for banshell
         zpkgs.scripts.npins-show        # npins-show command
 
         # Cursor Package
