99051a183afe3e00053d2611309f2e0aeeb13729
MeeSumee <stevemorrow69420@gmail.com>
2025-10-31 18:45:06 -0500

    more config

--- hosts/verdure/hardware-configuration.nix
+++ hosts/verdure/hardware-configuration.nix
@@ -1,1 +1,27 @@
-{}:{}
+{
+  lib, 
+  ... 
+}:
+{
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  # Set boot loader settings for RPi4B
+  boot.loader = {
+    efi.canTouchEfiVariables = lib.mkForce false;
+    systemd-boot.enable = lib.mkForce false;
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/";
+    fsType = "ext4";
+  };
+
+  swapDevices = [];
+
+  nixpkgs.hostPlatform = lib.mkForce "aarch64-linux";
+}
