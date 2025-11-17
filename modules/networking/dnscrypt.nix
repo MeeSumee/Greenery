28727ff1782f099d69b9c9ff2041a2c77f0eb8c4
MeeSumee <stevemorrow@proton.me>
2025-11-16 20:59:38 -0600

    add tiny core nas

--- modules/networking/dnscrypt.nix
+++ modules/networking/dnscrypt.nix
@@ -40,6 +40,7 @@
         cloaking_rules = pkgs.writeText "cloaking_rules.txt" ''
           greenery fd7a:115c:a1e0::8d34:ce04
           verdure fd7a:115c:a1e0::8034:cf55
+          seed 10.42.0.160
 
           kaolin fd7a:115c:a1e0::9034:6f42
           beryl fd7a:115c:a1e0::234:1456
