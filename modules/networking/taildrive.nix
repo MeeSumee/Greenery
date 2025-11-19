59e6d364861f1a68c11002f6249c6992e9f50e02
Sumee <stevemorrow69420@gmail.com>
2025-11-19 13:26:48 -0600

    add taildrive optimizations

--- modules/networking/taildrive.nix
+++ modules/networking/taildrive.nix
@@ -18,8 +18,12 @@
         "umask=003"
         "_netdev"
         "x-systemd.automount"
+        "compression=yes"
+        "cache=yes"
+        "auto_cache"
         "reconnect"
         "ServerAliveInterval=15"
+        "ServerAliveCountMax=3"
         "IdentityFile=${builtins.concatStringsSep "," (builtins.map (user: "${config.users.users.${user}.home}/.ssh/id_ed25519") users)}"
       ];
     };
