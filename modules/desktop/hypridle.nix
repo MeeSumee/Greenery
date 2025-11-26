6265f977ede42420c691de38bae052521283b2bd
MeeSumee <stevemorrow@proton.me>
2025-11-26 14:17:01 -0600

    revert changes

--- modules/desktop/hypridle.nix
+++ modules/desktop/hypridle.nix
@@ -1,14 +1,9 @@
 { 
   config, 
   lib,
-  pkgs,
-  inputs,
+  pkgs, 
   ... 
 }:{
-
-  imports = [
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
 
   options.greenery.desktop.hypridle.enable = lib.mkEnableOption "hypridle";
 
@@ -21,12 +16,11 @@
 
     # Fix paths
     systemd.user.services.hypridle.path = lib.mkForce (lib.attrValues {
-      inherit (pkgs inputs) brightnessctl systemd;
+      inherit (pkgs) brightnessctl systemd;
       hyprlock = config.programs.hyprlock.package;
       hyprland = config.programs.hyprland.package;
       niri = config.programs.niri.package;
       mango = config.programs.mango.package;
-      noctalia-shell = config.services.noctalia-shell.package;
     });
   };
 }
