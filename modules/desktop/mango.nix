7676de272bb75037052edad482f310350d74f281
MeeSumee <stevemorrow69420@gmail.com>
2025-11-30 21:58:16 -0600

    add spawns to config.conf as declaring in nix is unnecessary

--- modules/desktop/mango.nix
+++ modules/desktop/mango.nix
@@ -15,9 +15,6 @@
   config = lib.mkIf (config.greenery.desktop.mango.enable && config.greenery.desktop.enable) {
     programs.mango.enable = true;
 
-    security.polkit.enable = true;
-    programs.xwayland.enable = true;
-
     xdg.portal = {
       enable = true;
       wlr.enable = true;
@@ -44,7 +41,6 @@
 
           # Set x11 scaling
           echo "Xft.dpi: 255" | ${pkgs.xorg.xrdb}/bin/xrdb -merge
-          ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.5
 
           # wlsunset
           ${pkgs.wlsunset}/bin/wlsunset -T 3000 -t 2999 &
@@ -53,7 +49,7 @@
           ${noctalia} &
         '';
 
-        # Set mic mute toggle command
+        # Set keybind scripts
         keybind = let
           # camerascript = pkgs.writeShellScriptBin "camScript" ''
           #   if ${pkgs.kmod}/bin/lsmod | grep -q uvcvideo; then
@@ -70,47 +66,11 @@
             ${pkgs.brightnessctl}/bin/brightnessctl -d platform::micmute set $(${pkgs.wireplumber}/bin/wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -c MUTED)
           '';
 
-          appLauncher = ''
-            ${noctalia} ipc call launcher toggle
-          '';
-
-          calculator = ''
-            ${noctalia} ipc call launcher calculator
-          '';
-
-          lock = ''
-            ${noctalia} ipc call lockScreen lock
-          '';
-
-          visibleBar = ''
-            ${noctalia} ipc call bar toggle
-          '';
-
-          visibleDock = ''
-            ${noctalia} ipc call dock toggle
-          '';
-
-          screenRec = ''
-            ${noctalia} ipc call screenRecorder toggle
-          '';
-
           from = [
             "!STOP"
-            "@MAKING"
-            "#YOUR"
-            "$CONFIG"
-            "%SO"
-            "^PAINFUL"
-            "&REXCRAZY804"
           ];
           to = [
             "${mutescript}/bin/muteScript"
-            "${appLauncher}"
-            "${calculator}"
-            "${lock}"
-            "${visibleBar}"
-            "${visibleDock}"
-            "${screenRec}"
           ];
         in
           builtins.replaceStrings from to (builtins.readFile ../../dots/mango/config.conf);
