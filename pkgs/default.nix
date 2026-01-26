b6590a5106f324a90ebbc426cbe90e021450bc72
MeeSumee <stevemorrow69420@gmail.com>
2026-01-26 13:15:58 -0600

    graphite: excuse formatting to push entire nixos build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,11 +6,7 @@
   inherit (pkgs) callPackage;
 in {
   nahidacursor = callPackage ./cursors.nix {};
-
-  papiteal = pkgs.papirus-icon-theme.override {
-    color = "teal";
-  };
-
+  papiteal = pkgs.papirus-icon-theme.override { color = "teal"; };
   vesktop = pkgs.vesktop.override {
     withTTS = false;
     withMiddleClickScroll = true;
