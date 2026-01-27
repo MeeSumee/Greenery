bd0cc37a92747660078a1a65bbbabbb3d5fa58db
Sumee <stevemorrow69420@gmail.com>
2026-01-27 14:15:34 -0600

    greenery: secrets access policy changes

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -11,18 +11,19 @@ let
   };
   
   hosts = {
-    greenery = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiA3lkgGECrzk08GOhUlSIx5+jQ6WvuERK3nAz617M7"];
+    nahida = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiA3lkgGECrzk08GOhUlSIx5+jQ6WvuERK3nAz617M7"];
     quartz = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCHbUX3L2WxbS3d1EeCe+WhzdT1Tn78MzAzOz5xr1iO"];
     kaolin = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBLVW9nABsXKAakbIgzJdRWImREgUyxJ/8yOKpzbE9S"];
     beryl = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/j0qkT/J69KlyUNFx6sDILbCf4g20MxXBd10YDvwrd"];
     verdure = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdCRc7/VwBWConOyhV/3R9CiFK9RCaA04tGGuM+cOso"];
   };
 
+  # Principle of least privilege, other than Nahida <3
 in {
-  "secret1.age".publicKeys = hosts.greenery ++ hosts.verdure;
-  "secret2.age".publicKeys = hosts.greenery;
-  "secret3.age".publicKeys = hosts.greenery;
-  "secret4.age".publicKeys = hosts.verdure;
-  "secret5.age".publicKeys = hosts.kaolin;
-  "secret6.age".publicKeys = users.sumee;
+  "secret1.age".publicKeys = hosts.nahida ++ hosts.verdure;
+  "secret2.age".publicKeys = hosts.nahida;
+  "secret3.age".publicKeys = hosts.nahida;
+  "secret4.age".publicKeys = hosts.nahida ++ hosts.verdure;
+  "secret5.age".publicKeys = hosts.kaolin ++ hosts.nahida;
+  "secret6.age".publicKeys = users.sumee ++ hosts.nahida;
 }
