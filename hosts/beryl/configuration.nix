de93005dfe1792c583898f4cd0147924080476bb
MeeSumee <stevemorrow@proton.me>
2026-02-08 01:09:57 -0600

    quartz: oops for beryl

--- hosts/beryl/configuration.nix
+++ hosts/beryl/configuration.nix
@@ -1,17 +1,14 @@
 # Beryl Configuration
-{
-  lib,
-  ...
-}: {
+{lib, ...}: {
   imports = [
     # Imports.
     ../../modules
   ];
-  
+
   # All modules and their values
   greenery = {
     enable = true;
-    
+
     desktop = {
       enable = true;
       hypridle.enable = true;
@@ -31,7 +28,7 @@
 
     networking = {
       enable = true;
-      bluetooth.enable = true;      
+      bluetooth.enable = true;
       dnscrypt.enable = true;
       openssh.enable = true;
       taildrive.enable = true;
@@ -64,16 +61,13 @@
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
-  # Prioritize Fingerprint
-  programs.kurukuruDM.settings.instantAuth = lib.mkForce true;
-  
-/*
+  /*
   This value determines the NixOS release from which the default
   settings for stateful data, like file locations and database versions
   on your system were taken. It‘s perfectly fine and recommended to leave
   this value at the release version of the first install of this system.
   Before changing this value read the documentation for this option
   (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-*/
+  */
   system.stateVersion = "24.11"; # Did you read the comment?
 }
