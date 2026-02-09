454ec856f22b3dd68b5ca22a51535f60d001761e
MeeSumee <stevemorrow@proton.me>
2026-02-09 00:25:14 -0600

    quartz: update nvim config

--- modules/programs/nvim.nix
+++ modules/programs/nvim.nix
@@ -14,24 +14,40 @@
       enable = true;
       settings = {
         vim = {
+          syntaxHighlighting = true;
           viAlias = true;
           vimAlias = true;
+          autopairs.nvim-autopairs.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          binds.whichKey.enable = true;
+          git.enable = true;
           options.shiftwidth = 2;
+          treesitter.enable = true;
+          undoFile.enable = true;
+          visuals.indent-blankline.enable = true;
+
+          diagnostics = {
+            enable = true;
+            config.virtual_lines = true;
+          };
+
+          keymaps = [
+            {
+              key = "<A-o>";
+              mode = "n";
+              silent = true;
+              lua = true;
+              action = ''require("oil").toggle_float'';
+            }
+          ];
+
           lsp = {
             enable = true;
             formatOnSave = true;
             lightbulb.enable = true;
             trouble.enable = true;
           };
-          autopairs.nvim-autopairs.enable = true;
-          autocomplete.nvim-cmp.enable = true;
-          binds.whichKey.enable = true;
-          visuals.indent-blankline.enable = true;
-          git = {
-            enable = true;
-            gitsigns.enable = true;
-            gitsigns.codeActions.enable = false;
-          };
+
           languages = {
             enableFormat = true;
             enableDAP = true;
@@ -39,16 +55,20 @@
             enableExtraDiagnostics = true;
             bash.enable = true;
             clang.enable = true;
+            go.enable = true;
+            json.enable = true;
+            lua.enable = true;
             markdown.enable = true;
-            nix = {
-              enable = true;
-              format.enable = true;
-            };
+            nix.enable = true;
+            python.enable = true;
+            yaml.enable = true;
           };
+
           statusline.lualine = {
             enable = true;
             theme = "auto";
           };
+
           tabline = {
             nvimBufferline = {
               enable = true;
@@ -59,6 +79,7 @@
               };
               setupOpts = {
                 options = {
+                  diagnostics = false;
                   indicator.style = "none";
                   show_close_icon = false;
                   show_buffer_close_icons = false;
@@ -67,7 +88,7 @@
               };
             };
           };
-          treesitter.enable = true;
+
           telescope = {
             enable = true;
             mappings = {
@@ -79,39 +100,34 @@
               gitStatus = "<leader>gs";
             };
           };
+
           theme = {
             enable = true;
             name = "catppuccin";
             style = "mocha";
             transparent = true;
           };
-          keymaps = [
-            {
-              key = "<A-o>";
-              mode = "n";
-              silent = true;
-              lua = true;
-              action = ''require("oil").toggle_float'';
-            }
-          ];
+
           utility = {
-            motion.flash-nvim = {
-              enable = true;
-              mappings = {
-                jump = "<leader>s";
-                remote = "<leader>sr";
-                toggle = "<A-s>";
-                treesitter = "<leader>ss";
-                treesitter_search = "<leader>st";
-              };
-            };
             oil-nvim = {
               enable = true;
+              gitStatus.enable = true;
               setupOpts = {
                 delete_to_trash = true;
                 skip_confirm_for_simple_edits = true;
                 prompt_save_on_select_new_entry = true;
                 use_default_keymaps = false;
+                keymaps = {
+                  "?" = lib.mkLuaInline ''{ require("oil.actions").show_help, mode = "n" }'';
+                  "<Enter>" = lib.mkLuaInline ''{ require("oil.actions").select, mode = { "n", "v" } }'';
+                  "<BS>" = lib.mkLuaInline ''{ require("oil.actions").parent, mode = "n" }'';
+                  "." = lib.mkLuaInline ''require("oil.actions").toggle_hidden'';
+                  "t" = lib.mkLuaInline ''require("oil.actions").toggle_trash'';
+                  "q" = lib.mkLuaInline ''require("oil.actions").close'';
+                  "<Tab>" = lib.mkLuaInline ''require("oil.actions").open_cwd'';
+                  "`" = lib.mkLuaInline ''require("oil.actions").cd'';
+                  "<S-Enter>" = lib.mkLuaInline ''{ require("oil.actions").open_external, mode = "n" }'';
+                };
               };
             };
           };
