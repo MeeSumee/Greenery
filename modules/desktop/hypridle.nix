3446e179f51bd25a7026353df5890ed1f73ffb1f
MeeSumee <stevemorrow@proton.me>
2026-02-14 01:21:51 -0600

    quartz: fix hyprlock's long standing errors

--- modules/desktop/hypridle.nix
+++ modules/desktop/hypridle.nix
@@ -1,11 +1,10 @@
-{ 
-  config, 
+{
+  config,
   lib,
   pkgs,
   inputs,
-  ... 
-}:{
-
+  ...
+}: {
   imports = [
     inputs.noctalia.nixosModules.default
   ];
@@ -13,7 +12,6 @@
   options.greenery.desktop.hypridle.enable = lib.mkEnableOption "hypridle";
 
   config = lib.mkIf (config.greenery.desktop.hypridle.enable && config.greenery.desktop.enable) {
-
     # Hyprland idle daemon
     services.hypridle = {
       enable = true;
@@ -21,7 +19,7 @@
 
     # Fix paths
     systemd.user.services.hypridle.path = lib.mkForce (lib.attrValues {
-      inherit (pkgs) brightnessctl systemd;
+      inherit (pkgs) brightnessctl systemd coreutils;
       hyprlock = config.programs.hyprlock.package;
       hyprland = config.programs.hyprland.package;
       niri = config.programs.niri.package;
