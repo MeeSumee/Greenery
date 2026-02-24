1de0cdcdbdb7d6112f7ccba12942e2d11f016243
Sumee <stevemorrow69420@gmail.com>
2026-02-24 14:49:25 -0600

    greenery: improve screen

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,9 +1,8 @@
-{ 
+{
   lib,
   config,
-  ... 
+  ...
 }: {
-
   imports = [
     ./anki.nix
     ./auth.nix
@@ -15,12 +14,22 @@
     ./ollama.nix
     ./suwayomi.nix
   ];
-  
+
   options.greenery.server.enable = lib.mkEnableOption "enable server modules";
 
   config = lib.mkIf config.greenery.server.enable {
-
     # Enable GNU Screen
-    programs.screen.enable = true;
+    programs.screen = {
+      enable = true;
+      screenrc = ''
+        startup_message off
+        vbell off
+        altscreen on
+        termcapinfo rxvt* 'hs:ts=\E]2;:fs=\007:ds=\E]2;\007'
+        truecolor on
+        hardstatus off
+        hardstatus alwayslastline '%{#00ff00}[ %H ][%{#ffffff}%= %{7}%?%-Lw%?%{1;0}%{1}(%{15}%n%f%t%?(%u)%?%{1;0}%{1})%{7}%?%+Lw%? %=%{#00ff00}][ %{#00a5ff}%{6}%Y-%m-%d %{#ffffff}%{7}%0c%{#00ff00} ]'
+      '';
+    };
   };
 }
