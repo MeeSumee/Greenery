c028bd658a38ca4ca3d6a8eef3db038c6a4185a3
MeeSumee <stevemorrow@proton.me>
2026-03-14 01:52:18 -0500

    quartz: remove unused fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -4,19 +4,12 @@
   config,
   ...
 }: {
-
   options.greenery.system.fonts.enable = lib.mkEnableOption "fonts";
 
   config = lib.mkIf (config.greenery.system.fonts.enable && config.greenery.system.enable) {
-
     # Fonts
     fonts.packages = with pkgs; [
-      carlito
-      dejavu_fonts
-      ipafont
       kochi-substitute
-      source-code-pro
-      ttf_bitstream_vera
       nerd-fonts.caskaydia-mono
       nerd-fonts.caskaydia-cove
       material-symbols
