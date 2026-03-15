2e19b96cd92fc738d15a5fcce01248b0653d63ba
MeeSumee <stevemorrow69420@gmail.com>
2026-03-14 20:41:41 -0500

    beryl: fix ssh colorscheme

--- modules/networking/openssh.nix
+++ modules/networking/openssh.nix
@@ -3,20 +3,20 @@
   config,
   users,
   ...
-}:{
+}: {
   options.greenery.networking.openssh.enable = lib.mkEnableOption "openssh";
 
   config = lib.mkIf (config.greenery.networking.openssh.enable && config.greenery.networking.enable) {
-
     # Enable the OpenSSH daemon.
     services.openssh = {
       enable = true;
       startWhenNeeded = true;
       openFirewall = false;
-      
-      knownHosts = {
-        "greenery".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiA3lkgGECrzk08GOhUlSIx5+jQ6WvuERK3nAz617M7";
-      };
+
+      # Fix colors not showing
+      extraConfig = ''
+        AcceptEnv COLORTERM
+      '';
 
       settings = {
         PasswordAuthentication = false;
