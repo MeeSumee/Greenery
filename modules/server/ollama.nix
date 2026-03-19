904ea26f5bac3117b1fe6ba5e23969514eff62ce
MeeSumee <stevemorrow@proton.me>
2026-03-18 21:09:43 -0500

    quartz: harden ollama

--- modules/server/ollama.nix
+++ modules/server/ollama.nix
@@ -32,5 +32,20 @@
         };
       };
     };
+
+    # Harden model-loader
+    systemd.services.ollama-model-loader.serviceConfig = {
+      ProtectClock = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectKernelLogs = true;
+      SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+      ProtectControlGroups = true;
+      RestrictNamespaces = true;
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+    };
   };
 }
