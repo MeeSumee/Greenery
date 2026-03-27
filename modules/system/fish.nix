af317b73728a526ad74e3ea65a2ee5a58c3c0ad1
MeeSumee <stevemorrow@proton.me>
2026-03-26 20:55:28 -0500

    quartz: turns out I already have git autosign shit

--- modules/system/fish.nix
+++ modules/system/fish.nix
@@ -43,8 +43,7 @@ in {
           ga = "git add";
           gs = "git status";
           gc = "git commit";
-          gcm = ''git commit -m '';
-          gcs = ''git commit -S -m "$hostname:'';
+          gcm = ''git commit -m "$hostname:'';
           gck = "git checkout -b";
           gp = "git push";
           gpu = "git pull";
