9c534a4c157ee38862bad1b16049f03be211304d
MeeSumee <stevemorrow@proton.me>
2026-03-27 21:18:50 -0500

    quartz: make package of fprint follow upstream default

--- modules/hardware/fprint.nix
+++ modules/hardware/fprint.nix
@@ -10,7 +10,6 @@
     # Enable Fingerprint Sensor,
     services.fprintd = {
       enable = true;
-      package = pkgs.fprintd-tod;
       tod.enable = lib.mkDefault true;
       tod.driver = lib.mkDefault pkgs.libfprint-2-tod1-elan; # Elan 04f3:0c6e type fingerprint as default
     };
