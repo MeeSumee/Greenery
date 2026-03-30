ea57da7b3d6586f5385d6a486fabc5d6462fa086
MeeSumee <stevemorrow@proton.me>
2026-03-29 23:46:18 -0500

    quartz: finally fix home assistant with some compromises

--- modules/server/home.nix
+++ modules/server/home.nix
@@ -5,27 +5,30 @@
 }: {
   options.greenery.server.home.enable = lib.mkEnableOption "Home Assistant";
 
-  config =
-    lib.mkIf (config.greenery.server.home.enable && config.greenery.server.enable) {
-      services = {
-        home-assistant = {
-          enable = true;
-          extraComponents = [
-            "isal" # Faster Compression
-            "matter" # For my smart plug
+  config = lib.mkIf (config.greenery.server.home.enable && config.greenery.server.enable) {
+    # Note that this service runs on <system-host>.<tailnet-name>.ts.net
+    services.home-assistant = {
+      enable = true;
+      extraComponents = [
+        "isal" # Faster Compression
+        "matter" # For my smart plug
+      ];
+      config = {
+        http = {
+          server_port = 8123;
+          # Thanks https://community.home-assistant.io/t/home-assistant-400-bad-request-docker-proxy-solution/322163
+          use_x_forwarded_for = true;
+          trusted_proxies = [
+            "127.0.0.1"
+            "::1"
           ];
-          config = {
-            http.server_port = 8123;
-            homeassistant = {
-              unit_system = "metric";
-              time_zone = config.time.timeZone;
-              temperature_unit = "C";
-            };
-          };
         };
-
-        tailscale.serve.services.home.endpoints."tcp:443" = "https://127.0.0.1:${builtins.toString config.
-services.home-assistant.config.http.server_port}";
+        homeassistant = {
+          unit_system = "metric";
+          time_zone = config.time.timeZone;
+          temperature_unit = "C";
+        };
       };
     };
+  };
 }
