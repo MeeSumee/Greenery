0138bee2a526c2a8d0cc0c6c18b0e05907dcf9ae
MeeSumee <stevemorrow@proton.me>
2026-04-05 19:08:16 -0500

    quartz: reformat intelgpu

--- modules/hardware/intelgpu.nix
+++ modules/hardware/intelgpu.nix
@@ -3,15 +3,13 @@
   lib,
   config,
   ...
-}:{
-  
+}: {
   options.greenery.hardware.intelgpu.enable = lib.mkEnableOption "intel graphics";
 
   config = lib.mkIf (config.greenery.hardware.intelgpu.enable && config.greenery.hardware.enable) {
-
     # Failsafe if FFMPEG/QSV fails to initialize
-    boot.kernelParams = [ "i915.enable_guc=3" ];
-    
+    boot.kernelParams = ["i915.enable_guc=3"];
+
     # Intel graphics packages
     hardware = {
       enableRedistributableFirmware = true;
