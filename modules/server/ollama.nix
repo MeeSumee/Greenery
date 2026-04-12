8e88a158505221580d009e4d630f52f60cff1295
MeeSumee <stevemorrow@proton.me>
2026-04-12 14:01:57 -0500

    quartz: ollama updated so I can use gemma4

--- modules/server/ollama.nix
+++ modules/server/ollama.nix
@@ -13,7 +13,8 @@
         package = pkgs.ollama-rocm;
         syncModels = true;
         loadModels = [
-          "gemma3:12b"
+          "gemma4:e4b"
+          "qwen3.5:9b"
           "deepseek-r1:14b"
         ];
       };
