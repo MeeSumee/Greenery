951b35ac192047c07b5a807c47401a122ecf4568
MeeSumee <stevemorrow69420@gmail.com>
2026-04-25 17:31:10 -0500

    rename generic cursors.nix to stmc.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,7 +7,7 @@ lib.fix (self: let
   inherit (pkgs) callPackage;
   revision = "v0.0.0-20260106222316-bb080c4414ac";
 in {
-  nahidacursor = callPackage ./cursors.nix {};
+  nahidacursor = callPackage ./stmc.nix {};
   davinci = callPackage ./davinci.nix {};
   papiteal = pkgs.papirus-icon-theme.override {color = "teal";};
   dickord = pkgs.equibop.override {
