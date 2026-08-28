83af462c5b660ca5ef9f42f8dc7aa62cd376a6ff
MeeSumee <stevemorrow@proton.me>
2026-08-28 18:36:55 -0500

    update caddy-tailscale

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,10 @@
 }:
 lib.fix (self: let
   inherit (pkgs) callPackage;
-  revision = "v0.0.0-20260106222316-bb080c4414ac";
+  # NOTE TO SELF
+  # If you're unsure what the new revision format is, just type main or random characters
+  # and the caddy build will tell you the correct revision
+  revision = "v0.0.0-20260826180304-de41b249af4f";
 in {
   stmc-cursor = callPackage ./stmc.nix {};
   davinci = callPackage ./davinci.nix {};
@@ -18,6 +21,6 @@ in {
       "github.com/tailscale/caddy-tailscale@${revision}"
       # "github.com/caddy-dns/cloudflare@v0.2.4"
     ];
-    hash = "sha256-vC/nyCKMD2jKgbGVA5NIJP6dGXiP9z0yEA8WINgFcVc=";
+    hash = "sha256-tR+Da52Ozvwtk7LcCM9DFmDwcKxLd/u+2VmOTs1JFsI=";
   };
 })
